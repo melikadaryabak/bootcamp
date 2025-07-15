a862f0e99d04c3d69c2dd00f9a57694118bd9aab
Melika <melikadaryaback@gmail.com>
2025-07-15 21:16:44 +0430

    code

--- cmd/bootcamp/main.go
+++ cmd/bootcamp/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"database/sql"
 	"log"
-
+	"net/http"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/melikadaryabak/bootcamp/internal/application/services"
 	"github.com/melikadaryabak/bootcamp/internal/handler"
 )
 
 func main() {
+
+	 
+   // Static file handler
+   http.Handle("/", http.FileServer(http.Dir("./static")))
+
+
 	// Connect to MySQL
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/amacodecamp")
 
@@ -20,10 +27,11 @@ func main() {
 		log.Fatalf("Error pinging database: %v", err)
 	}
 
-	// Check database connection
-	if err != nil {
-		log.Fatalf("Error opening database connection: %v", err)
-	}
+	// // Check database connection
+	// if err != nil {
+	// 	log.Fatalf("Error opening database connection: %v", err)
+	// }
+
 
 	srvc := services.NewServices(db)
 
